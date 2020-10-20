ead915770ac00d19c0a2cedf8967edf925931cce
alvarobarrio <65124639+alvarobarrio@users.noreply.github.com>
2020-10-20 16:14:21 +0200

    Code

--- Code.R
+++ Code.R
@@ -15,36 +15,9 @@ library(tidyr)
 library(DT)
 library(scales)
 
-install.packages("webshot")
-webshot::install_phantomjs()
-
-tinytex::install_tinytex()
-update.packages(ask = FALSE, checkBuilt = TRUE)
-tinytex::tlmgr_update()
-tinytex::reinstall_tinytex()
-tinytex:::install_prebuilt()
-options(tinytex.verbose = TRUE)
-
-
-
-
-
-library(ggplot2)
-library(ggthemes)
-library(lubridate)
-library(dplyr)
-library(tidyr)
-library(DT)
-library(scales)
-
-
-
 
 
 ### 2. Creación de vector de colores para implementar en nuestras parcelas
-
-
-
 colors = c("#CC1011", "#665555", "#05a399", "#cfcaca", "#f5e840", "#0683c9", "#e075b0")
 
 
